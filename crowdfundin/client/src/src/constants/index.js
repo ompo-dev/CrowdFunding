10c403a64778003568568cb4df779a46589c7970
Maicon Pereira <106182410+ompo-dev@users.noreply.github.com>
2023-03-13 15:30:17 -0300

    Crowdfunding - Web3.0
    Crowdfunding - Web 3.0 - React Js - Vite - Solidity

---
+++ crowdfundin/client/src/src/constants/index.js
@@ -0,0 +1,35 @@
+import { createCampaign, dashboard, logout, payment, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/dashboard',
+  },
+  {
+    name: 'campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+  },
+  {
+    name: 'payment',
+    imgUrl: payment,
+    link: '/payment',
+  },
+  {
+    name: 'withdraw',
+    imgUrl: withdraw,
+    link: '/withdraw',
+  },
+  {
+    name: 'profile',
+    imgUrl: profile,
+    link: '/profile',
+  },
+  {
+    name: 'logout',
+    imgUrl: logout,
+    link: '/logout',
+    disabled: true,
+  },
+];
