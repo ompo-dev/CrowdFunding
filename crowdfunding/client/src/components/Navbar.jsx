25eb28efd7775ce0a7391ab00cd7024f471aa0e0
2326mpb <2326mpb@gmail.com>
2024-06-17 23:52:56 +0000

    rename

--- crowdfunding/client/src/components/Navbar.jsx
+++ crowdfunding/client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
 import { useStateContext } from '../context';
-import { CustomButton } from './';
+import { CustomButton } from '.';
 import { logo, menu, search, thirdweb } from '../assets';
 import { navlinks } from '../constants';
 
